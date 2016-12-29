66a1168bf56c5075fc5b689f196100710c7de98f
Tae Hoon Lee <thlee1122@hotmail.com>
2016-12-29 00:24:43 -0500

    Revised

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,8 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
 	return (
-		<h3>Examples</h3>,
-		<p>Welcome to examples page!!</p>
+		<div>
+			<h3>Examples</h3>
+			<p>Welcome to examples page!!</p>
+		</div>
 	)
 }
 
