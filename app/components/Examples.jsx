01c8a3ef2696acf7353e0004a57ca88ae5673b65
Tae Hoon Lee <thlee1122@hotmail.com>
2016-12-29 14:13:54 -0500

    Modified Main Page and Example Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 // var Examples = React.createClass({
 // 	render: function() {
@@ -11,8 +12,17 @@ var React = require('react');
 var Examples = (props) => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to examples page!!</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here a few example locations to try out:</p>
+			<ol>
+				<li>
+					<Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+				</li>
+
+				<li>
+					<Link to='/?location=Rio'>Rio, Brazil</Link>
+				</li>
+			</ol>
 		</div>
 	)
 }
